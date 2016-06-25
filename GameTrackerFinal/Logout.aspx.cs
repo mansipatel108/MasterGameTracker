284754b617c2103f0afd5b137d382ce9bde08563
Divya <divyaraval25@outlook.com>
2016-06-24 18:15:13 -0700

    Add project files.

---
+++ GameTrackerFinal/Logout.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GameTrackerFinal
+{
+    public partial class Logout : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //store session info and authentication methods in the authnticationManager object
+            var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+
+            //perform signout
+            authenticationManager.SignOut();
+
+            //redirect to the home page
+            Response.Redirect("~/Game.aspx");
+        }
+    }
+}
